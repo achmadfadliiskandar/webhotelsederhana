e991bd89bcad5463e20caab00a13d5a1eebaadd9
achmadfadliiskandar <af137357@gmail.com>
2022-07-26 10:24:00 +0700

    silahkan di download dan dicustom

--- resources/views/tamu/laporanbooking.blade.php
+++ resources/views/tamu/laporanbooking.blade.php
@@ -5,150 +5,108 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Kode Bukti Pembayaran : {{$kamarorder->booking_kode}}</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
+<style>
+    h2{
+        font-weight: 500;
+        font-size: 50px;
+    }
+</style>
 <body>
-    <h1 class="judul">Bukti Pembayaran : {{$kamarorder->user->name}}</h1>
-    <p class="kalimat2">Jagalah Kartu / PDF ini jangan sampai hilang sebagai bukti untuk pemesanan kamar</p>
-    <div class="buktipesan">
-        <table class="table">
-            <thead>
-            <tr>
-                <th scope="col">No</th>
-                <th scope="col">Kamar Yang di pesan</th>
-                <th scope="col">Tanggal Checkin</th>
-                <th scope="col">Tanggal Checkout</th>
-            </tr>
-            </thead>
-            <tbody>
-                @php
-                    $no = 1
-                @endphp
-                @foreach ($kamarorder->detailkamarorder as $item)
-                    <tr>
-                        <td>{{$no++}}</td>
-                        <td>{{$item->kamars->tipe_kamar->tipe_kamar}}</td>
-                        <td>{{$item->tanggal_checkin}}</td>
-                        <td>{{$item->tanggal_checkout}}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-    <div class="pembayaran">
-        <h2 class="judul">Detail Pembayaran</h2>
-        <p class="kalimat2">informasi mengenai pembayaran ada di bawah ini</p>
-        <ul>
+    <h2 style="text-transform:uppercase;color:#4b0082;">invoice</h2>
+    {{-- <br> --}}
+    <hr>
+    <p>Alamat : Jalan Kopo No 27 Beji Depok</p>
+    <p>Kode Pos : 16412</p>
+    <p>Email : <a href="indigo@gmail.com">indigo@gmail.com</a></p>
+    <p>Website : <a href="https://webhotelbelajar.achmadf1.my.id/" target="_blank">https://webhotelbelajar.achmadf1.my.id/</a></p>
+    <span style="float: right;margin-top:-150px;margin-right:77px;">Nama Pemesan : {{$kamarorder->user->name}}</span>
+    <br>
+    <span style="float: right;margin-right:-10px;margin-top:-150px;">Email Pemesan : {{$kamarorder->user->email}}</span>
+    <br>
+    <span style="float: right;margin-right:85px;margin-top:-150px;">Kode Invoice : {{$kamarorder->booking_kode}}</span>
+    <br>
+    <span style="float: right;margin-right:-30px;margin-top:-150px;">Tanggal Order : {{$kamarorder->user->created_at}}</span>
+    <br>
+    <span style="float: right;margin-right:-30px;margin-top:-150px;">Due Date : {{$kamarorder->updated_at}}</span>
+    <h3>Informasi Kamar</h3>
+    <div class="alert alert-info">Bawalah Bukti Ini pada saat checkin dan disarankan datang saat hari menginap tiba</div>
+    <table class="table table-bordered">
+        <thead>
+        <tr>
+            <th scope="col">No</th>
+            <th scope="col">Kamar Yang di pesan</th>
+            <th scope="col">Tanggal Checkin</th>
+            <th scope="col">Tanggal Checkout</th>
+        </tr>
+        </thead>
+        <tbody>
             @php
+                $no = 1
+            @endphp
+            @foreach ($kamarorder->detailkamarorder as $item)
+                <tr>
+                    <td>{{$no++}}</td>
+                    <td>{{$item->kamars->tipe_kamar->tipe_kamar}}</td>
+                    <td>{{$item->tanggal_checkin}}</td>
+                    <td>{{$item->tanggal_checkout}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+    <div class="card" style="width: 100%;">
+        <div class="card-header">
+            Informasi Pembayaran
+    </div>
+    <ul class="list-group list-group-flush">
+        @php
                 $hargas = 0;
             @endphp
             @foreach ($kamarorder->detailkamarorder as $item)
-            <li class="first">Nama Kamar : {{$item->kamars->tipe_kamar->tipe_kamar}}</li>
-            <li>Harga Kamar Per Malam : {{number_format($item->kamars->hargakamarpermalam,-2,".",".")}}</li>
-            <li>Lama Menginap : {{$item->lama_menginap}}</li>
-            <li>Dp : {{number_format($item->dp_dibayar,-2,".",".")}}</li>
-            <li>Total Harga : {{number_format($item->totalharga,-2,".",".")}}</li>
+            <li class="list-group-item">Nama Kamar : {{$item->kamars->tipe_kamar->tipe_kamar}}</li>
+            <li class="list-group-item">Harga Kamar Per Malam : {{number_format($item->kamars->hargakamarpermalam,-2,".",".")}}</li>
+            <li class="list-group-item">Lama Menginap : {{$item->lama_menginap}}</li>
+            <li class="list-group-item">Dp : {{number_format($item->dp_dibayar,-2,".",".")}}</li>
+            <li class="list-group-item">Total Harga : {{number_format($item->totalharga,-2,".",".")}}</li>
             @php
                 $hargas+=$item->totalharga;
             @endphp
-            <li>Keterangan : 
+            <li class="list-group-item">Keterangan : 
                 @if ($item->tanggal_checkin < date("Y-m-d"))
                 <strong style="color: red;">Tidak bisa dipakai Karena Sudah lewat waktu</strong>
                 @else
                 <strong style="color: blue;">Masih Bisa dipakai</strong>
                 @endif
             </li>
-            @endforeach 
-        </ul>
-    </div>
-    <div class="judul">Total Keseluruhan yang harus di bayarkan : 
-    @php
-        echo number_format($hargas,-2,".",".");
-    @endphp
-    </div>
-    <div class="pemberitahuan">Untuk Pembayaran Dan Pengubahan status silahkan datang ke resepsionis</div>
-    <div class="kolomkonfirmasi">
-        <h2>Status Pembayaran 
+            <li class="list-group-item">Status Pembayaran
             @if ($kamarorder->statuspembayaran == 'belumlunas')
-                {{"belum lunas"}}
+            :{{"belum lunas"}}
             @else
-            {{$kamarorder->statuspembayaran}} 
-            @endif
-        </h2>
-        <h4>
+            :{{$kamarorder->statuspembayaran}} 
+            @endif | Konfirmasi Pembayaran
             @if ($kamarorder->status == 'uncorfirmed')
-                {{"status belum terkonfirmasi"}}
+            : {{"status belum terkonfirmasi"}}
             @else
-            {{$kamarorder->status}}
+            : {{$kamarorder->status}}
             @endif
-        </h4>
-        <h3>
+            </li>
+            <li class="list-group-item">Keterangan Pembayaran
+            @if ($kamarorder->metodepembayaran == NULL)
+            : <span style="color: red;">Belum Ada Metode Pembayaran yang digunakan</span>
+            @else
+            : {{$kamarorder->metodepembayaran}}
+            @endif
+            | Jumlah Pembayaran
             @if ($kamarorder->jumlahdibayar == NULL)
-                <span style="color: red;">Belum Dibayar</span>
+            : <span style="color: red;">Belum Dibayar</span>
             @else
-            {{number_format($kamarorder->jumlahdibayar,-2,".",".")}}
+            : {{number_format($kamarorder->jumlahdibayar,-2,".",".")}}
             @endif
-        </h3>
-        <h3>
-        @if ($kamarorder->metodepembayaran == NULL)
-            <span style="color: red;">Belum Ada Metode Pembayaran yang digunakan</span>
-        @else
-        {{$kamarorder->metodepembayaran}}
-        @endif
-        </h3>
-    </div>
+            </li>
+            @endforeach 
+    </ul>
+    {{-- </div> --}}
 </body>
 </html>
 
-<style>
-    .judul{
-        font-family:Arial, Helvetica, sans-serif;
-        text-align: center;
-    }
-    .kalimat2{
-        text-align: center;
-        font-family:Arial, Helvetica, sans-serif;
-    }
-    .buktipesan{
-        overflow-x:auto; 
-    }
-    .pemberitahuan{
-        font-family:Arial, Helvetica, sans-serif;
-        padding-top:5px;
-        text-align: center;
-    }
-    .table{
-        text-align: center;
-        /* border: 1px solid black; */
-        /* width: 100%; */
-        height: 100px;
-        font-family:Arial, Helvetica, sans-serif;
-        border-collapse: collapse;
-        width: 100%;
-    }
-    tr:nth-child(even) {
-    background-color: #dddddd;
-    }
-    td , th{
-        padding: 8px;
-    }
-    th{
-        border: 1px solid black;
-    }
-    td{
-        border: 1px solid black;
-    }
-    ul{
-        list-style-type: none;
-        padding: 0;
-        margin: 0;
-    }
-    ul li {
-        border: 1px solid #ddd;
-        margin-top: -1px;
-        background-color: #f6f6f6;
-        padding: 12px;
-    }
-    .first{
-        background-color: #4adeae;
-    }
-</style>
